cfb8c37f6894131c5d8da30d0c4d63da9bbddcd2
Jared Bada <jared.bada@student.teau.ac.ke>
2024-03-29 23:30:14 +0300

    initial commit on develop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -10,4 +10,19 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'iafrica-ltd';
+  scrollAmount = 10;
+  // Listens to the scroll of the website
+@HostListener('window:wheel', ['$event'])
+scroll_event($event: any) {
+  $event.preventDefault();
+
+  window.scrollTo(0, Math.E * 2 * this.scrollAmount);
+
+  // Scrolls up or down
+  if ($event.deltaY > 0) {
+    this.scrollAmount++;
+  } else {
+    this.scrollAmount--;
+  }
+}
 }
