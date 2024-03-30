d892f24fde507f4dbb80aeb2e12a3e6728f4e119
Jared Bada <jared.bada@student.teau.ac.ke>
2024-03-30 20:16:59 +0300

    working on home

--- src/app/core/layout/footer/footer.component.ts
+++ src/app/core/layout/footer/footer.component.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'iafrica-footer',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
-
+  navContent = [
+    {
+      title: `Public Relations`,
+      path: `services/public-relations`,
+    },{
+      title: `Media Planning`,
+      path: `services/media-planning`,
+    },{
+      title: `Advertising`,
+      path: `services/advertising`,
+    },{
+      title: `Market Research`,
+      path: `services/market-research`,
+    },{
+      title: `Event Management`,
+      path: `services/public-relations`,
+    },{
+      title: `Experiential Marketing`,
+      path: `services/experiential-marketing`,
+    },
+  ];
 }
